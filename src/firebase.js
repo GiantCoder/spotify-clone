cc5d142e3b67b98182eac0040f1902f5dfc7bdad
Matt O'Toole <mattjotoole@gmail.com>
2021-10-09 16:26:48 +0100

    Config files and dotenv.

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB9FLFUPRrn2C1aZEkfaCQ7D95exEkow6k",
+  authDomain: "spotify-65b80.firebaseapp.com",
+  projectId: "spotify-65b80",
+  storageBucket: "spotify-65b80.appspot.com",
+  messagingSenderId: "418275637693",
+  appId: "1:418275637693:web:d4e46dc1dbd46ecc563ad4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
